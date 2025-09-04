49e804628fbe64f8222f24010c2372a90b66428e
Sherzod267 <ataxonovsherzod77@gmail.com>
2025-09-04 18:07:41 +0500

    11

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          router: ['react-router-dom'],
+          lottie: ['lottie-react']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000,
+    sourcemap: false
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react-router-dom', 'lottie-react']
+  }
 })
