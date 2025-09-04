1a38567fd12cf698fe7fc3b87c3c7f6ebc2ac1be
Sherzod267 <ataxonovsherzod77@gmail.com>
2025-09-04 17:57:29 +0500

    ss

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,29 +1,27 @@
 import './Header.css'
 import { Link } from 'react-router-dom'
-import logo from '../assets/code.png'
+import logo from '/code.png' // agar public/ da bo'lmasa
 
-function header() {
+function Header() {
   return (
-   <header className='home'>
-    <div className="headerLogo">
-      <img className='logo' src={logo} alt="logo" />
-      <div className='title-container'>
-      <h1>Sherzod</h1>
-      <p>web</p>
+    <header className='home'>
+      <div className="headerLogo">
+        <img className="logo" src={logo} alt="logo" />
+        <div className='title-container'>
+          <h1>Sherzod</h1>
+          <p>web</p>
+        </div>
       </div>
-    </div>
-    <nav className='navbar'>
-      <ul>
-        <li><Link to ='/'>Home</Link></li>
-        <li><Link to ='/about'>About</Link></li>
-        <li><Link to ='/countries'>Countries</Link></li>
-        <li><Link to ='/goals'>Goals</Link></li>
-        <li><Link to ='/contact'>Contact</Link></li>
-      </ul>
-
-    </nav>
-   </header>
+      <nav className='navbar'>
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/about'>About</Link></li>
+          <li><Link to='/goals'>Goals</Link></li>
+          <li><Link to='/contact'>Contact</Link></li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
-export default header
+export default Header
